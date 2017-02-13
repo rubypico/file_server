788e817e973f13942adf0211faa457ef7fb7f0ed
ongaeshi <ongaeshi0621@gmail.com>
2017-02-14 00:14:55 +0900

    Extract FileServer class

--- file_server.rb
+++ file_server.rb
@@ -1,30 +1,38 @@
 require 'simplehttpserver'
 
-server = SimpleHttpServer.new({
-  :server_ip => "0.0.0.0",
-  :port  =>  8000,
-  :document_root => File.join(Dir.documents, "public"),
-  # :debug => true,
-})
+class FileServer
+  attr_reader :server
 
-server.http do |r|
-  server.set_response_headers({
-    "Server" => "file_server",
-    "Date" => server.http_date,
-  })
-end
-
-server.location "/" do |r|
-  path = File.join server.config[:document_root], r.path
-
-  if File.directory? path
-    server.set_response_headers "Content-type" => "text/html; charset=utf-8"
-
-    files = Dir.entries(path).map do |e|
-      "    <li><a href=\"#{File.join r.path, e}\">#{e}</a></li>"
+  def initialize(opt = nil)
+    unless opt
+      opt = {
+        :server_ip => "0.0.0.0",
+        :port  =>  8000,
+        :document_root => File.join(Dir.documents, "public"),
+        # :debug => true,
+      }
     end
 
-    server.response_body = <<EOS
+    @server = SimpleHttpServer.new(opt)
+
+    @server.http do |r|
+      @server.set_response_headers({
+                                     "Server" => "file_server",
+                                     "Date" => @server.http_date,
+                                   })
+    end
+
+    @server.location "/" do |r|
+      path = File.join @server.config[:document_root], r.path
+
+      if File.directory? path
+        @server.set_response_headers "Content-type" => "text/html; charset=utf-8"
+
+        files = Dir.entries(path).map do |e|
+          "    <li><a href=\"#{File.join r.path, e}\">#{e}</a></li>"
+        end
+
+        @server.response_body = <<EOS
 <html>
 <head>
   <title>#{r.path}</title>
@@ -38,22 +46,39 @@ server.location "/" do |r|
 </html>
 EOS
 
-    server.create_response
-  else
-    server.file_response r, path, "text/plain"
+        @server.create_response
+      else
+        @server.file_response r, path, "text/plain"
+      end
+    end
+  end
+  
+  def url
+    @server.url
+  end
+  
+  def config
+    @server.config
+  end
+  
+  def run
+    @server.run
   end
 end
 
-unless File.exists? server.config[:document_root]
-  puts <<EOS
+if __FILE__ == $0
+  server = FileServer.new
+
+  unless File.exists? server.config[:document_root]
+    puts <<EOS
 Not found server directory at "#{server.config[:document_root]}".
 
 Please create the directory and put the files you want to share.
 
 Or change the :document_root parameter.
 EOS
-else
-  puts server.url
-  server.run
+  else
+    puts server.url
+    server.run
+  end
 end
-
